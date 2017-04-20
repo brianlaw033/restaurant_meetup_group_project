74f39fccb8e6d1692a10f9b10732194dbedcca68
brianlaw033 <brian9328hk@gmail.com>
2017-04-20 11:14:38 +0800

    create new migration files for new criterias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418031137) do
+ActiveRecord::Schema.define(version: 20170420031149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,12 @@ ActiveRecord::Schema.define(version: 20170418031137) do
     t.string   "image"
   end
 
+  create_table "timeslots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "time"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
@@ -65,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170418031137) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "gender"
   end
 
 end
