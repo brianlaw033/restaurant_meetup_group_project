66930d575012d1f1367782319d35a11fbb742eab
brianlaw033 <brian9328hk@gmail.com>
2017-04-22 18:06:10 +0800

    styling

--- lib/user.rb
+++ lib/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
       users = User
       .where(:gender => self.gender_preference)
       .where.not(id: self.id)
-      .where.not(id: self.match_as_user2.map{|match| match.user2_id})
+      .where.not(id: self.match_as_user1.map{|match| match.user2_id})
       .shuffle()
     else
       users = User
